2b43bd67240ace50ca72cc8af6d0791b4c0b8404
mngaw20 <mngaw20@gmail.com>
2015-10-07 18:42:12 -0400

    Setup UI w/ Twitter Bootstrap
    added title, static count, input, static to-do, remove button

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var app = angular.module('countApp', []);
+var app = angular.module('todoApp', []);
 
-app.controller('countController', function() {
-    var count = this;
-
-    count.number = 0;
-    count.addOne = function () {
-        count.number++;
-    };
+app.controller('mainController', function() {
+    
 });
