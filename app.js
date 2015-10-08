adc60db855142659a928cda734f4d457533d548a
mngaw20 <mngaw20@gmail.com>
2015-10-08 16:43:15 -0400

    Final | To-Do App w/ Full Functionality
    Can take in to-do items, delete selected to-do items. Also counts
    number of to-do items.

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var app = angular.module('todoApp', []);
 
-app.controller('mainController', function() {
-    
-});
+app.controller('mainController', ['$scope', function($scope) {
+
+    $scope.formText = '';
+
+    $scope.todos = [{text: 'Get Milk', completed: false}];
+
+    $scope.todosLength = function() {
+        return $scope.todos.length;
+    };
+
+    $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.formText, completed: false});
+        $scope.formText = '';
+    };
+
+    $scope.removeTodos = function () {
+        var oldTodos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldTodos, function(todo) {
+            if(!todo.completed) { $scope.todos.push(todo); }
+        });
+    };
+
+}]);
